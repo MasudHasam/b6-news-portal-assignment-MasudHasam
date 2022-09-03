7d4a4cc75264026573639a3f65409a8f3665bfdb
Masud Hasan <masudhasamf@gmail.com>
2022-09-03 15:31:38 +0600

    modal details button connected

--- scripts/index.js
+++ scripts/index.js
@@ -41,7 +41,6 @@ const displayCategorysNews = (allNews) => {
     const newsPost = document.getElementById('newsPost');
     newsPost.innerHTML = '';
     allNews.forEach(sigleItem => {
-        console.log(sigleItem);
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card mb-3" style="max-width: 840px;">
@@ -54,30 +53,41 @@ const displayCategorysNews = (allNews) => {
                     <h5 class="font-bold">${sigleItem.title}</h5>
                     <p class="text-alipsis pt-2">${sigleItem.details}</p>
 
-                <div class="d-flex justify-content-between align-items-center mt-2 px-3">
-
-                 <div class="d-flex align-items-center gap-2">
-                      <div><img class="rounded-full" id="authorImg" src="${sigleItem.author.img}" alt=""></div>
-                      <div>
-                         <small class="font-semibold block text-gray-500">${sigleItem.author.name ? sigleItem.author.name : 'not found'}</small>
-                         <smalll class="font-small text-gray-500">${sigleItem.author.published_date ? sigleItem.author.published_date.slice(0, 10) : 'not found'}</small>
-                      </div>
-                 </div>
-
+                  <div class="d-flex justify-content-between align-items-center mt-2 px-3">
+                     <div class="d-flex align-items-center gap-2">
+                        <div><img class="rounded-full" id="authorImg" src="${sigleItem.author.img}" alt=""></div>
+                        <div>
+                           <small class="font-semibold block text-gray-500">${sigleItem.author.name ? sigleItem.author.name : 'not found'}</small>
+                           <smalll class="font-small text-gray-500">${sigleItem.author.published_date ? sigleItem.author.published_date.slice(0, 10) : 'not found'}</small>
+                        </div>
+                     </div>
                      <div class="d-flex gap-2 pt-3">
                            <div><i class="lni lni-eye"></i></div>
                            <div>
                               <small class="font-bold block">${sigleItem.total_view ? sigleItem.total_view : 'not found'}</small>
                            </div>
                      </div>
-
-                 <div class="pt-3  text-4xl "><button><i class="lni lni-arrow-right-circle"></i></button></div>
+                     <div class="pt-3  text-4xl "><button onclick="detailsButton('${sigleItem._id}')"  data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="lni lni-arrow-right-circle"></i></button></div>
+                 </div>
                </div>
             </div>
         </div>
-    </div>
         `;
         newsPost.appendChild(div);
     });
 }
+
+
+const detailsButton = async (itemId) => {
+    const url = `https://openapi.programming-hero.com/api/news/${itemId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setModalDetails(data.data[0]);
+};
+
+
+const setModalDetails = (itemDetails) => {
+    console.log(itemDetails.total_view);
+}
+
 loadCatagories();
